6f0270d3c3cf2166aa7287ec26f719391cec62ba
Jon Grim <jonjongrim@gmail.com>
2022-12-14 22:36:30 -0500

    update image domain and fix local markers (#6)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    domains: [
+      "https://vacation-roulette.vercel.app/",
+      "https://vacation-roulette-jonjongrim.vercel.app/",
+    ],
     remotePatterns: [
       {
         protocol: "https",
